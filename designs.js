b21c0bff9cdab2c93e07c927b9276240881d590e
Gideon W <gow210@stern.nyu.edu>
2018-07-09 19:52:56 -0400

    Define Variables

--- designs.js
+++ designs.js
@@ -1,5 +1,12 @@
 // Select color input
+const chooseColor = document.getElementById('#colorPicker')
+
 // Select size input
+const height = document.querySelector('#inputHeight')
+const width = document.querySelector('#inputWidth')
+
+const chooseSize = document.querySelector('#sizePicker')
+const canvas = document.querySelector('#pixelCanvas')
 
 // When size is submitted by the user, call makeGrid()
 

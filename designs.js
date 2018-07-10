b96f4daa839bbbaaf348c109b21e2848046ba51e
Gideon W <gow210@stern.nyu.edu>
2018-07-09 21:10:25 -0400

    Final submission

--- designs.js
+++ designs.js
@@ -1,5 +1,5 @@
 // Select color input
-const chooseColor = document.getElementById('#colorPicker')
+const chooseColor = document.querySelector('#colorPicker')
 
 // Select size input
 const height = document.querySelector('#inputHeight')
@@ -10,34 +10,39 @@ const canvas = document.querySelector('#pixelCanvas')
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function clearOldGrid() {
   canvas.innerHTML = '';
-  let tr, td;
+}
 
+  function makeGrid() {
+  let tr, td;
   //create the table, for loops for creating the table rows and cells,
   // then appending
-   for (let i=0; i<height.value; i++) {
+   for (let i = 0; i < height.value; i++) {
      tr = document.createElement('tr');
      canvas.appendChild(tr);
-     for (let k=0; k<width.value; k++) {
+     for (let k = 0; k < width.value; k++) {
        td = document.createElement('td');
        tr.appendChild(td)
-     }
+     }}
    }
 /**
-oh, by the way, each time when the button is clicked, the user should
-see a new grid. So the first line in the function’s body is used
-for clean up existing grid,
-The user needs to change the color of a cell, how to make it happen?
-Ok, so here’s a thing, because every element inherits properties
-from the Node Interface, which means each td has the .nodeName
-property of “TD”, and I use event.target to reach the exact element,
+The user needs to change the color of a cell, and I use event.target to reach the exact element,
 and use if function to determine whether the condition is fulfilled
 **/
+function changeCellColor() {
 canvas.addEventListener('click', function(e) {
   e.preventDefault();
-  if (e.target.nodeName==='TD') {
-    e.target.style.backgroundColor = color.value;
+  if (e.target.nodeName === 'TD') {
+    e.target.style.backgroundColor = chooseColor.value;
   }
 })
 }
+
+//react to the submit button
+chooseSize.addEventListener('submit', function(e) {
+  e.preventDefault();
+  clearOldGrid();
+  makeGrid();
+  changeCellColor();
+})
